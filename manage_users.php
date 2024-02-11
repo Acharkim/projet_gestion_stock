a81e825ec8d50df0913c3f5c44f63276c6df67ab
Acharkim <159682932+Acharkim@users.noreply.github.com>
2024-02-11 09:36:24 +0100

    Update manage_users.php

--- manage_users.php
+++ manage_users.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html>
 <head>
     <title>...::: Gestion des Utilisateurs :::...</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form method="post" action="">
